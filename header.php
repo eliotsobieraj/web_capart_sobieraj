fac95d34da579506bef3ecbc728fc4451b87ee26
eliotsobieraj <123156287+eliotsobieraj@users.noreply.github.com>
2023-02-19 22:53:29 +0100

    first header file

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="menutop.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="headercontain">
+<div class="header">
+<div class="menubar">
+    <button class="menubtn"></button>
+    <button class="menubtn"></button>
+    <button class="menubtn"></button>
+</div>
+    <form action="" method="post" >
+        <button class="searchbtn" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search " viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg></button>
+        <input type="search" class="searchbar" placeholder="search">
+
+    </form>
+    <img class="madamephoto" src="image/madamephoto.png" alt="">
+</div>
+</div>
+<div id="triangle"></div>
+<div style="height: 1000px"></div>
+</body>
+</html>
