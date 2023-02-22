6ddf09034c11e7581b84bbbc3b72521529423757
qualite <117730700+mathiscapart@users.noreply.github.com>
2023-02-22 15:54:42 +0100

    change css of index

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $id->execute();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="image/1f4f7.png">
     <link rel="stylesheet" type="text/css" href="style_index.css">
     <title>Page d'accueil</title>
 </head>
