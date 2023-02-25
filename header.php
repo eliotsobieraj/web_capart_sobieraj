b8019c02cf5ddfa33a38c94054aea5a8e7c71f40
eliotsobieraj <123156287+eliotsobieraj@users.noreply.github.com>
2023-02-25 22:43:46 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -11,27 +11,28 @@
 </head>
 <body>
 <div class="headercontain">
-<div class="header">
-<div class="menubar">
-    <button class="menubtn"></button>
-    <button class="menubtn"></button>
-    <button class="menubtn"></button>
-</div>
-    <form action="" method="post" >
-        <button class="searchbtn" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search " viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg></button>
-        <input type="search" class="searchbar" placeholder="search">
 
-    </form>
-    <img class="madamephoto" src="image/madamephoto.png" alt="">
+
+    <div class="respmenucont">
+        <button class="menubtnresp"><a href="index.php">Accueil</a></button>
+        <button class="menubtnresp"><a href="upload.php">Upload</a></button>
+        <button class="menubtnresp">Contact</button></div>
+    <div class="header">
+        <div class="menubar">
+            <button class="menubtn"><a href="index.php">Accueil</a></button>
+            <button class="menubtn"><a href="upload.php">Upload</a></button>
+            <button class="menubtn">Contact</button>
+        </div>
+        <form action="" method="post" >
+            <button class="searchbtn" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search " viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg></button>
+            <input type="search" class="searchbar" placeholder="search">
+        </form>
+        <img class="madamephoto" src="image/madamephoto.png" alt="">
+    </div>
+    <p id="accueil">Welcome on WePost</p>
 </div>
-</div>
-<form action="" method="post">
-<button class="startuploading">Start uploading</button>
-</form>
-<h1 class="maintitle">Weload</h1>
 <div id="triangle"></div>
-<div style="height: 1000px"></div>
 </body>
 </html>
---
+++ modification.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="image/1f4f7.png">
+    <link href="style_modification.css" type="text/css" rel="stylesheet">
+    <title>Modification</title>
+</head>
+<body>
+    <?php
+        include "header.php";
+        try {
+            $bd = new PDO("mysql:host=localhost;dbname=wepost;charset=utf8", 'root' , 'root');
+        }catch (Exception $e){
+            echo "Erreur :" . $e->getMessage();
+        }
+
+        $id = $bd->prepare('SELECT * FROM photos WHERE id = ?');
+        $id->execute(array($_GET['id']));
+        $article = $id->fetch(PDO::FETCH_ASSOC);
+        $id_article = $id->fetchColumn();
+    ?>
+    <div id="box">
+        <form method="post" action="" enctype="multipart/form-data" class="form">
+            <div>
+                <h3>Article</h3>
+                <button type="submit" class="retour"><a href="index.php">Retour</a></button>
+            </div>
+            <label for="titre">Enter titre</label><br><input type="text" name="titre_article" id="titre" value="<?php echo htmlspecialchars($article['title'])?>"><br>
+            <label for="pseudo">Enter pseudo</label><br><input type="text" name="pseudo_article" id="pseudo" value="<?php echo htmlspecialchars($article['pseudo'])?>"><br>
+            <label id="picture">Choose a picture:</label><br>
+            <input type="file" id="image" name="article_image" accept="image/png, image/jpeg"><br>
+            <button type="submit" id="ajouter">Modifier</button>
+        </form>
+        <?php
+            if(!empty($_POST['titre_article']) && !empty($_FILES['article_image']) && !empty($_POST['pseudo_article'])){
+                $titre_article = $_POST['titre_article'];
+                $pseudo_article = $_POST['pseudo_article'];
+
+                if($_FILES['article_image']['error'] == 0 && in_array(pathinfo($_FILES['article_image']['name'])['extension'], ['jpg', 'jpeg', 'png'])){
+                    move_uploaded_file(htmlspecialchars($_FILES['article_image']['tmp_name']), "image/" . date("d-m-y") .$_FILES["article_image"]["name"]);
+                }
+
+                $rss = $bd->prepare('UPDATE photos SET pseudo=?, title=?, location=?, file_name=?, created_at=NOW() WHERE id = ?');
+                $rss->execute(array($pseudo_article, $titre_article, "image/". date("d-m-y") . $_FILES["article_image"]["name"], $_FILES["article_image"]["name"], $id_article));
+                echo "<p>Votre article a été modifié</p>";
+            }else{
+                echo "<p>Votre article n'a pas été modifié</p>";
+            }
+        ?>
+    </div>
+</body>
+</html>
+
